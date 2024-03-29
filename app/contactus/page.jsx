282f696486a4959e428aba29fe207b2ca321b6cb
Nadun <gimhanapc2@gmail.com>
2024-03-29 17:26:47 +0530

    structure changes 1

--- app/contactus/page.jsx
+++ app/contactus/page.jsx
@@ -2,25 +2,38 @@
 import { SingleVehicleContext } from "@/context/SingleVehicalContextProvider";
 import React, { useContext } from "react";
 
-
 const Contact = () => {
   const { vehicle, setVehical } = useContext(SingleVehicleContext);
   return (
     <>
-      <div className="bg-slate-400  h-[300px] flex flex-col">
-        <h1 className="text-center text-[50px] pt-[70px] font-semibold">Contact Us</h1>
+      <div className="bg-slate-400  h-[400px] flex flex-col">
+        <h1 className="text-center text-[50px] pt-[70px] font-semibold">
+          Contact Us
+        </h1>
       </div>
 
-       <div className="flex flex-row justify-center pt-[50px] gap-10 ">
-        <div className="bg-green-300 p-10 rounded"><h1>Address</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesettin
-           industry. Lorem Ipsum has been the industry
-           's standard dummy te
+      <div className="flex flex-row justify-center pt-[50px] gap-10">
+        <div className="bg-blue-300/50 p-10 rounded">
+          <h1 className="text-center text-2xl pb-3">Address</h1>
+          <p className="text-center">
+            Lorem Ipsum is simply dummy <br /> text of the printing <br />
+            and typesettin
           </p>
-        
         </div>
-        <div className="bg-green-300 p-10"><h1>Phone</h1></div>
-        <div className="bg-green-300 p-10"><h1>E-mail</h1></div>
+        <div className="bg-blue-300/50 p-10 rounded">
+          <h1 className="text-center text-2xl pb-3">Phone</h1>
+          <p className="text-center">
+            Lorem Ipsum is simply dummy <br /> text of the printing <br />
+            and typesettin
+          </p>
+        </div>
+        <div className="bg-blue-300/50 p-10 rounded">
+          <h1 className="text-center text-2xl pb-3">E-mail</h1>
+          <p className="text-center">
+            Lorem Ipsum is simply dummy <br /> text of the printing <br />
+            and typesettin
+          </p>
+        </div>
       </div>
     </>
   );
