d90d563cb582d13cc26f4a348b69bac5a65e7727
Nadun <gimhanapc2@gmail.com>
2024-03-29 12:54:25 +0530

    structure

--- app/contactus/page.jsx
+++ app/contactus/page.jsx
@@ -1,19 +1,27 @@
 "use client";
 import { SingleVehicleContext } from "@/context/SingleVehicalContextProvider";
-import Link from "next/link";
 import React, { useContext } from "react";
+
 
 const Contact = () => {
   const { vehicle, setVehical } = useContext(SingleVehicleContext);
   return (
     <>
-      <div className="my-3">page contact</div>
+      <div className="bg-slate-400  h-[300px] flex flex-col">
+        <h1 className="text-center text-[50px] pt-[70px] font-semibold">Contact Us</h1>
+      </div>
 
-      <div className="my-3 bg-yellow-300">{vehicle.type}</div>
-
-      <Link href={"/"} className="p-3 bg-green-300">
-        CLICK HERE tO HOME
-      </Link>
+       <div className="flex flex-row justify-center pt-[50px] gap-10 ">
+        <div className="bg-green-300 p-10 rounded"><h1>Address</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesettin
+           industry. Lorem Ipsum has been the industry
+           's standard dummy te
+          </p>
+        
+        </div>
+        <div className="bg-green-300 p-10"><h1>Phone</h1></div>
+        <div className="bg-green-300 p-10"><h1>E-mail</h1></div>
+      </div>
     </>
   );
 };
