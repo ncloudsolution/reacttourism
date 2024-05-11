5047c909c7140eb759e94d36d7535a4933c680be
Sohan Prabhath <soizsinghe@gmail.com>
2024-05-11 19:29:38 +0530

    email modified

--- components/emailTemplates/BrandNew.jsx
+++ components/emailTemplates/BrandNew.jsx
@@ -24,21 +24,28 @@ export const BrandNew = ({ tourDetails }) => {
       <Head />
       <Preview>Yelp recent login</Preview>
       <Body style={main}>
-        <Container>
+        <Container style={{ maxWidth: "680px", backgroundColor: "#FDFDFD" }}>
           {/* <Section style={logo}>
-                <Img src={`${baseUrl}/static/yelp-logo.png`} />
-              </Section> */}
+            <Img src={`${baseUrl}/static/yelp-logo.png`} />
+          </Section> */}
 
           <Section style={content}>
             {/* <Row>
-                  <Img
-                    style={image}
-                    width={620}
-                    src={`${baseUrl}/static/yelp-header.png`}
-                  />
-                </Row> */}
+              <Img
+                style={image}
+                width={620}
+                src={`${baseUrl}/static/yelp-header.png`}
+              />
+            </Row> */}
 
-            <Row style={{ ...boxInfos, paddingBottom: "0" }}>
+            <Row
+              style={{
+                ...boxInfos,
+                paddingBottom: "0",
+                width: "100%",
+                margin: "0 auto",
+              }}
+            >
               <Column>
                 <Heading
                   style={{
@@ -52,214 +59,234 @@ export const BrandNew = ({ tourDetails }) => {
                   New Order Received
                 </Heading>
                 {/* <Heading
-                      as="h2"
-                      style={{
-                        fontSize: 26,
-                        fontWeight: "bold",
-                        textAlign: "center",
-                      }}
-                    >
-                      We noticed a recent login to your Yelp account.
-                    </Heading> */}
+                  as="h2"
+                  style={{
+                    fontSize: 26,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                  }}
+                >
+                  We noticed a recent login to your Yelp account.
+                </Heading> */}
                 {/**IDENTITY DETAILS**/}
-                <Text style={{ ...paragraph, marginTop: 30 }}>
-                  <b>Customer Name : </b>
-                  {tourDetails.customerName}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Customer Email : </b>
-                  {tourDetails.customerEmail}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Mobile No : </b>
-                  {tourDetails.customerMobileNo}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Whatsapp No : </b>
-                  {tourDetails.customerWhatsappMobileNo}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>NIC / Passport No : </b>
-                  {tourDetails.customerNicPassport}
-                </Text>
+                <Section style={section}>
+                  <Text style={paragraph}>
+                    <b>Customer Name : </b>
+                    {tourDetails.customerName}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Customer Email : </b>
+                    {tourDetails.customerEmail}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Mobile No : </b>
+                    {tourDetails.customerMobileNo}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Whatsapp No : </b>
+                    {tourDetails.customerWhatsappMobileNo}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>NIC / Passport No : </b>
+                    {tourDetails.customerNicPassport}
+                  </Text>
+                </Section>
 
                 {/**FLIGHT DETAILS**/}
-                <Text style={{ ...paragraph, marginTop: 20 }}>
-                  <b>Flight No : </b>
-                  {tourDetails.customerFlightNo}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Arrival Date : </b>
-                  {tourDetails.arrivalDate}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Arrival Time : </b>
-                  {tourDetails.arrivalTime}
-                </Text>
-                {tourDetails.cusDisplayName != "" && (
-                  <Text style={{ ...paragraph, marginTop: -15 }}>
-                    <b>Board Name : </b>
-                    {tourDetails.cusDisplayName}
+                <Section style={section}>
+                  <Text style={paragraph}>
+                    <b>Flight No : </b>
+                    {tourDetails.customerFlightNo}
                   </Text>
-                )}
+                  <Text style={paragraph}>
+                    <b>Arrival Date : </b>
+                    {tourDetails.arrivalDate}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Arrival Time : </b>
+                    {tourDetails.arrivalTime}
+                  </Text>
+                  {tourDetails.cusDisplayName != "" && (
+                    <Text style={paragraph}>
+                      <b>Board Name : </b>
+                      {tourDetails.cusDisplayName}
+                    </Text>
+                  )}
+                </Section>
 
                 {/**JOURNEY DETAILS**/}
-                <Text style={{ ...paragraph, marginTop: 20 }}>
-                  <b>Origin : </b>
-                  {tourDetails.origin}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Destination : </b>
-                  {tourDetails.destination}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Start Date : </b>
-                  {tourDetails.startDate}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Start Time : </b>
-                  {tourDetails.startTime}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Return Date : </b>
-                  {tourDetails.returnDate}
-                </Text>
-                {tourDetails.returnTime && (
-                  <Text style={{ ...paragraph, marginTop: -15 }}>
-                    <b>Return Time : </b>
-                    {tourDetails.returnTime}
+                <Section style={section}>
+                  <Text style={paragraph}>
+                    <b>Origin : </b>
+                    {tourDetails.origin}
                   </Text>
-                )}
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Distance : </b>
-                  {tourDetails.distance}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Duration : </b>
-                  {tourDetails.duration}
-                </Text>
+                  <Text style={paragraph}>
+                    <b>Destination : </b>
+                    {tourDetails.destination}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Start Date : </b>
+                    {tourDetails.startDate}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Start Time : </b>
+                    {tourDetails.startTime}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Return Date : </b>
+                    {tourDetails.returnDate}
+                  </Text>
+                  {tourDetails.returnTime && (
+                    <Text style={paragraph}>
+                      <b>Return Time : </b>
+                      {tourDetails.returnTime}
+                    </Text>
+                  )}
+                  <Text style={paragraph}>
+                    <b>Distance : </b>
+                    {tourDetails.distance}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Duration : </b>
+                    {tourDetails.duration}
+                  </Text>
+                </Section>
 
                 {/**PRICING SECTION**/}
-                <Text style={{ ...paragraph, marginTop: 20 }}>
-                  <b>Selected Vehical : </b>
-                  {tourDetails.vehicleType}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>No.of Passengers : </b>
-                  {tourDetails.noOfPassengers}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Luggage Count : </b>
-                  {tourDetails.customerLuggageCount}
-                </Text>
-                <Text style={{ ...paragraph, marginTop: -15 }}>
-                  <b>Vehical Price: </b>
-                  {tourDetails.converedCurrencySymbol}{" "}
-                  {tourDetails.convertedPrice}
-                </Text>
-                {tourDetails.boardShow != 0 && (
-                  <Text style={{ ...paragraph, marginTop: -15 }}>
-                    <b>Board Show: </b>
-                    {tourDetails.converedCurrencySymbol}{" "}
-                    {(
-                      tourDetails.boardShow * tourDetails.conversionRate
-                    ).toFixed(2)}
+                <Section style={section}>
+                  <Text style={paragraph}>
+                    <b>Selected Vehical : </b>
+                    {tourDetails.vehicleType}
                   </Text>
-                )}
-
-                {tourDetails.highwayExit &&
-                  tourDetails.highwayExit !== "None" &&
-                  tourDetails.highwayCharge !== "No any Charge" && (
-                    <Text style={{ ...paragraph, marginTop: -15 }}>
-                      <b>Highway Charges: </b>
+                  <Text style={paragraph}>
+                    <b>No.of Passengers : </b>
+                    {tourDetails.noOfPassengers}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Luggage Count : </b>
+                    {tourDetails.customerLuggageCount}
+                  </Text>
+                  <Text style={paragraph}>
+                    <b>Vehical Price: </b>
+                    {tourDetails.converedCurrencySymbol}{" "}
+                    {tourDetails.convertedPrice}
+                  </Text>
+                  {tourDetails.boardShow != 0 && (
+                    <Text style={paragraph}>
+                      <b>Board Show: </b>
                       {tourDetails.converedCurrencySymbol}{" "}
                       {(
-                        tourDetails.highwayCharge * tourDetails.conversionRate
+                        tourDetails.boardShow * tourDetails.conversionRate
                       ).toFixed(2)}
                     </Text>
                   )}
 
-                {tourDetails.highwayExit !== "None" && (
-                  <Text style={{ ...paragraph, marginTop: -15 }}>
-                    <b>Higway Exit : </b>
-                    {tourDetails.highwayExit}
-                  </Text>
-                )}
+                  {tourDetails.highwayExit &&
+                    tourDetails.highwayExit !== "None" &&
+                    tourDetails.highwayCharge !== "No any Charge" && (
+                      <Text style={paragraph}>
+                        <b>Highway Charges: </b>
+                        {tourDetails.converedCurrencySymbol}{" "}
+                        {(
+                          tourDetails.highwayCharge * tourDetails.conversionRate
+                        ).toFixed(2)}
+                      </Text>
+                    )}
+
+                  {tourDetails.highwayExit !== "None" && (
+                    <Text style={paragraph}>
+                      <b>Higway Exit : </b>
+                      {tourDetails.highwayExit}
+                    </Text>
+                  )}
+                </Section>
 
                 {/**TOTAL SECTION**/}
+                <Section style={section}>
+                  <Text style={paragraph}>
+                    <b>Total Price : </b>
+                    {tourDetails.converedCurrencySymbol} {""}
+                    {tourDetails.totalPrice}
+                  </Text>
 
-                <Text style={{ ...paragraph, marginTop: 20 }}>
-                  <b>Total Price : </b>
-                  {tourDetails.converedCurrencySymbol} {""}
-                  {tourDetails.totalPrice}
-                </Text>
-
-                <Text style={{ ...paragraph, marginTop: -5 }}>
-                  <span
+                  <div
                     style={{
-                      backgroundColor: "#eab308",
-                      padding: "10px",
-                      borderRadius: "5px",
+                      ...paragraph,
+                      marginTop: 5,
+                      display: "flex",
+                      width: "fit-content",
                     }}
                   >
-                    <b>Total Price in LKR : </b>
-                    <span>{`Rs. ${tourDetails.totalPriceInLkr}`}</span>{" "}
-                  </span>
-                </Text>
-
-                {/* <Text
+                    <span
                       style={{
-                        color: "rgb(0,0,0, 0.5)",
-                        fontSize: 14,
-                        marginTop: -5,
+                        backgroundColor: "#eab308",
+                        padding: "10px",
+                        borderRadius: "5px",
                       }}
                     >
-                      *Approximate geographic location based on IP address:
-                      {tourDetails.converedCurrencySymbol}
-                    </Text> */}
+                      <b>Total Price in LKR : </b>
+                      <span>{`Rs. ${tourDetails.totalPriceInLkr}`}</span>{" "}
+                    </span>
+                  </div>
+                </Section>
 
-                <Text
+                {/* <Text
                   style={{
-                    fontSize: 18,
                     color: "rgb(0,0,0, 0.5)",
-                    marginTop: 40,
+                    fontSize: 14,
+                    marginTop: -5,
                   }}
                 >
-                  Connect The Customer
-                </Text>
+                  *Approximate geographic location based on IP address:
+                  {tourDetails.converedCurrencySymbol}
+                </Text> */}
 
-                {/* <Text style={{ ...paragraph, marginTop: -5 }}>
-                      If this wasn't you or if you have additional questions, please
-                      see our support page.
-                    </Text> */}
+                <Section style={section}>
+                  <Text
+                    style={{
+                      ...paragraph,
+                      fontSize: 18,
+                      color: "rgb(0,0,0, 0.5)",
+                      borderTop: "4px solid #eab308",
+                      padding: "20px 0px",
+                    }}
+                  >
+                    Connect The Customer
+                  </Text>
 
-                <Row style={{ ...boxInfos, paddingTop: "0" }}>
-                  {/* <Column style={containerButton} colSpan={2}> */}
-                  <Button
-                    style={{ ...button, margin: "0px 10px 20px 0px" }}
-                    href={`mailto:${tourDetails.customerEmail}`}
-                  >
-                    Email
-                  </Button>
-                  <Button
-                    style={{ ...button, backgroundColor: "#075e54" }}
-                    href={`https://wa.me/${tourDetails.customerWhatsappMobileNo}`}
-                  >
-                    WhatsApp
-                  </Button>
-                  {/* </Column> */}
-                </Row>
+                  {/* <Text style={{ ...paragraph, marginTop: -5 }}>
+                  If this wasn't you or if you have additional questions, please
+                  see our support page.
+                </Text> */}
+
+                  <Row style={{ paddingTop: "0" }}>
+                    {/* <Column style={containerButton} colSpan={2}> */}
+                    <Button
+                      style={{ ...button, margin: "0px 10px 20px 0px" }}
+                      href={`mailto:${tourDetails.customerEmail}`}
+                    >
+                      Email
+                    </Button>
+                    <Button
+                      style={{ ...button, backgroundColor: "#075e54" }}
+                      href={`https://wa.me/${tourDetails.customerWhatsappMobileNo}`}
+                    >
+                      WhatsApp
+                    </Button>
+                    {/* </Column> */}
+                  </Row>
+                </Section>
               </Column>
             </Row>
           </Section>
 
           {/* <Section style={containerImageFooter}>
-                <Img
-                  style={image}
-                  width={620}
-                  src={`${baseUrl}/static/yelp-footer.png`}
-                />
-              </Section> */}
+            <Img
+              style={image}
+              width={620}
+              src={`${baseUrl}/static/yelp-footer.png`}
+            />
+          </Section> */}
 
           <Text
             style={{
@@ -285,8 +312,14 @@ const main = {
     '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
 };
 
+const section = {
+  marginBottom: 20,
+};
+
 const paragraph = {
-  fontSize: 16,
+  fontSize: 14,
+  margin: 0,
+  color: "black",
 };
 
 const logo = {
@@ -313,6 +346,7 @@ const content = {
   border: "1px solid rgb(0,0,0, 0.1)",
   borderRadius: "3px",
   overflow: "hidden",
+  width: "100%",
 };
 
 const image = {
