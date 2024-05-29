417408c7a01968a0677eca0dee6766e67145814e
Sohan Prabhath <soizsinghe@gmail.com>
2024-05-29 15:31:58 +0530

    meta data added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,12 @@
 import AboutUs from "@/components/AboutUs";
 import React from "react";
 
+export const metadata = {
+  title: "About Us",
+  description:
+    "Welcome to our premier transportation service in Sri Lanka, where we cater to all your travel needs with professionalism and care. From seamless airport transfers and point-to-point transportation to curated day trips, comprehensive tour packages, scenic rail journeys, and custom trips, we ensure a smooth and enjoyable journey across this beautiful island. Our well-maintained fleet of vehicles, competitive rates, and commitment to punctuality guarantee a hassle-free experience. Trust us to provide exceptional service from the moment you arrive, making your journey through Sri Lanka unforgettable.",
+};
+
 function page() {
   return (
     <>
--- app/contactus/page.jsx
+++ app/contactus/page.jsx
@@ -2,6 +2,12 @@ import ContactUsForm from "@/components/ContactUsForm";
 import dynamic from "next/dynamic";
 import React from "react";
 
+export const metadata = {
+  title: "Contact Us",
+  description:
+    "We value your inquiries and are here to assist you with all your travel needs in Sri Lanka. Whether you have questions about our airport transfers, point-to-point transportation, day trips, tour packages, rail journeys, or custom trips, our dedicated team is ready to help. Please feel free to reach out to us anytime. Our friendly and professional staff are available 24/7 to provide you with detailed information, address your concerns, and ensure your travel experience is exceptional. Contact us today and let us help make your journey through Sri Lanka seamless and memorable",
+};
+
 const Contact = () => {
   return (
     <>
