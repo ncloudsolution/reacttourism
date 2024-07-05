0907af27e1c6ea64c462ae7ed6977810f7977339
Sohan Prabhath <soizsinghe@gmail.com>
2024-07-05 17:42:25 +0530

    start map rendering 10% all tour packages 100% over

--- app/(testingroutes)/newtesting/page.jsx
+++ app/(testingroutes)/newtesting/page.jsx
@@ -1,58 +1,34 @@
-"use client";
-
-import CheckBoxContainer from "@/components/CheckBoxContainer";
-import DayTripTimer from "@/components/DayTripTimer";
-import RadioBtnContainer from "@/components/RadioBtnContainer";
-import CustomHotelDropDown from "@/components/standalone/CustomHotelDropDown";
 import React, { useContext } from "react";
-import customTourTransport from "@/data/customTourTransport.json";
-import { GetCustomTourCheckedItems } from "@/libs/JsonDataCatching";
-import { TourContext } from "@/context/TourContextProvider";
+
+import {
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
 const Page = () => {
-  const { tourDetails, setTourDetails } = useContext(TourContext);
-  // const tourDetails = {
-  //   customerName: "sohan",
-  //   customerEmail: "a@gmail.com",
-  //   customerMobileNo: 77,
-  //   customerWhatsappMobileNo: 55,
-  //   customerNicPassport: 1,
-  //   customerFlightNo: 1,
-  //   arrivalDate: 1,
+  async function calculateRoute() {
+    const origin = "Colombo";
+    const destination = ["padukka", "Ingiriya"];
 
-  //   cusDisplayName: 1,
-  //   origin: 1,
-  //   destination: 1,
-  //   startDate: new Date(),
+    let directionResponse;
 
-  //   returnDate: new Date(),
+    try {
+      const directionService = new google.maps.DirectionsService();
+      const results = await directionService.route({
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+      });
 
-  //   distance: 1,
-  //   duration: 1,
-  //   vehicleType: 1,
-  //   noOfPassengers: 1,
-  //   customerLuggageCount: 1,
-  //   converedCurrencySymbol: 1,
-  //   convertedPrice: 1,
-  //   conversionRate: 1,
+      directionResponse = results;
+    } catch (error) {
+      console.error("Error occurred while route rendering:", error);
+    }
+  }
 
-  //   boardShow: 1,
-  //   highwayExit: 1,
-  //   highwayCharge: 1,
-  //   totalPrice: 1,
-  //   totalPriceInLkr: 1,
-  // };
-  return (
-    <div>
-      <CheckBoxContainer
-        contextName="transportationRequirement"
-        identifier="type"
-        Selectfunction={GetCustomTourCheckedItems([...customTourTransport])}
-      />
-
-      <div>sss {tourDetails.transportationRequirement?.join(", ")}</div>
-    </div>
-  );
+  return <div>HELLO</div>;
 };
 
 export default Page;
